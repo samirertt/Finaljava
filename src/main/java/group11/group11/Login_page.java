4dd94914018aece5857ce58cb69749d0db8d22e0
samirertt <samirertt@gmail.com>
2024-12-28 23:38:29 +0300

    beginning

---
+++ src/main/java/group11/group11/Login_page.java
@@ -0,0 +1,34 @@
+package group11.group11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Login_page extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load the FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/loginPage.fxml"));
+            Parent root = loader.load();
+
+            // Set up the stage and scene
+            primaryStage.setTitle("Login Page");
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+
+            // Show the stage
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error: Unable to load the FXML file. Ensure the path and controller are correct.");
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module group11.group11 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens group11.group11 to javafx.fxml;
+    exports group11.group11;
+}
